76bfa5e8e25a217d13a9cc3d86ab72013dbbe55a
Natalia Ivantsova <ivantsovana@gmail.com>
2024-07-07 13:12:34 +0300

    feat: add scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,30 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'react-compiler', '@typescript-eslint', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react-compiler/react-compiler': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^React$' },
+    ],
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
