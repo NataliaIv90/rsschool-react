6eeda4ed369e5f9632c75117c6f7fc74bc981963
Natalia Ivantsova <ivantsovana@gmail.com>
2024-08-05 12:12:11 +0300

    refactor: remove commented lines

--- src/components/searchPage/SearchPage.test.tsx
+++ src/components/searchPage/SearchPage.test.tsx
@@ -3,19 +3,11 @@ import { screen } from '@testing-library/react';
 
 import { renderWithProviders } from '@/tests/renderWithProviders';
 import { SearchPage } from './SearchPage';
-// import { useGetListDataQuery } from '@/redux/slices';
 
 vi.mock('../routeError/RouteError', () => ({
   RouteError: vi.fn(({ currentError }) => <div>{currentError.message}</div>),
 }));
 
-// vi.mock('../../redux/slices/starWarsApiSlice', async (importOriginal) => {
-//   const actual: object = await importOriginal();
-//   return {
-//     ...actual,
-//     useGetListDataQuery: vi.fn(),
-//   };
-// });
 const useGetListDataQuery = vi.fn();
 vi.mock('next/router', () => ({
   useRouter: () => ({
